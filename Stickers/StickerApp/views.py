360b3387b9aa0ab6e7d70cf2553061622d8f56b2
Ariadna Nasisi <ari.nasisi@gmail.com>
2022-10-17 00:36:27 -0300

    se agrega el primer template, una ruta al index y una ruta parametrizada de prueba de selección de un sticker del catalogo

--- Stickers/StickerApp/views.py
+++ Stickers/StickerApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index (request):
+    return render (request, "index.html")
+
+def individual (request, id):
+    return HttpResponse(f'item {id}')
