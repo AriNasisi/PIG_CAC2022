1c91c1e5816ca162143927138eb6213ae1a15ddb
Juan Corica <juan_corica@hotmail.com>
2022-10-18 00:49:31 -0300

    agregamos herencia de templates, vista parametrizada, lista de stickers con for en django, link a cada vista individual de la lista

--- Stickers/StickerApp/urls.py
+++ Stickers/StickerApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('item/<str:id>', views.individual),
+    path('', views.index, name='index'),
+    path('item/<str:name>', views.individual, name='individual'),
     
 
 ]
--- Stickers/StickerApp/views.py
+++ Stickers/StickerApp/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index (request):
-    return render (request, "index.html")
 
-def individual (request, id):
-    return HttpResponse(f'item {id}')
+stickers = ['Primer Stk', 'Segundo Stk', 'Tercer Stk', 'Cuarto Stk', 'Quinto Stk', 'Sexto Stk']
+
+def index (request):
+    
+    context = {'stickers': stickers, 'titulo': 'Catálogo'}
+    return render (request, "index.html", context)
+
+def individual (request, name):
+    id = stickers.index(name)
+    individual = stickers[id]
+    context = {'stickers': stickers, 'titulo': id+1, 'individual': individual}
+    return render (request, "individual.html", context)
