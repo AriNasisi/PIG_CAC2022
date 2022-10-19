43cb79f5b852338b1230eb4d3a371b3cab5dabd6
Ariadna Nasisi <ari.nasisi@gmail.com>
2022-10-18 21:19:25 -0300

    se agrega imagen de fondo y archivo css con estilos. Se corrigen las views

--- Stickers/StickerApp/views.py
+++ Stickers/StickerApp/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 stickers = ['Primer Stk', 'Segundo Stk', 'Tercer Stk', 'Cuarto Stk', 'Quinto Stk', 'Sexto Stk']
 
 def index (request):
-    
     context = {'stickers': stickers, 'titulo': 'Catálogo'}
     return render (request, "index.html", context)
 
 def individual (request, name):
-    id = stickers.index(name)
-    individual = stickers[id]
-    context = {'stickers': stickers, 'titulo': individual, 'individual': individual}
+    context = {'stickers': stickers, 'titulo': name}
     return render (request, "individual.html", context)
 
 def novedades (request):
