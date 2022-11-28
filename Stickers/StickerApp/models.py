c3c57c64c15b017e5ff5c358b125d47e624547a6
Juan Corica <juan_corica@hotmail.com>
2022-11-27 22:37:05 -0300

    se comienza con modelos y se modifica footer

--- Stickers/StickerApp/models.py
+++ Stickers/StickerApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Sticker(models.Model):
+    nombre: models.CharField(max_length=20, verbose_name='Nombre')
+    imagen:
+        
+    def __str__(self):
+        return self.nombre
+
+
+class Pedido(models.Model):
+    nombre:
