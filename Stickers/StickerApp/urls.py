bebdcaedb7500d415136fba5b71368963a8135c8
Juan Corica <juan_corica@hotmail.com>
2022-12-10 18:29:49 -0300

    cambios menores

--- Stickers/StickerApp/urls.py
+++ Stickers/StickerApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('carrito/', views.carrito, name='carrito'),
     
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
